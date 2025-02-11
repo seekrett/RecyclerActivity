f3ba35df5ad541bd25cc41d125a90639f2c4b2d3
Yuto <63772196+seekrett@users.noreply.github.com>
2025-02-11 12:13:45 -0500

    created recycler view of numbers

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,37 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {
+        val numberView = textView
+    }
+
+    // create the view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(
+                parent.context
+            ).apply {
+                setPadding(10,10,10,10)
+            }
+        )
+    }
+
+    // populate the view
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position].toString()
+        holder.textView.textSize = numbers[position].toFloat()
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
 
     // TODO Step 3b: Complete function definitions for adapter
 
